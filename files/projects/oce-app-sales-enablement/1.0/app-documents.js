5707b15e964ff506582a19c11851b37a465fad1c
john_money <john.money@oracle.com>
2020-12-01 15:03:22 -0500

    my documents

--- files/projects/oce-app-sales-enablement/1.0/app-documents.js
+++ files/projects/oce-app-sales-enablement/1.0/app-documents.js
@@ -5,24 +5,29 @@
   //https://docs.oracle.com/en/cloud/paas/content-cloud/rest-api-documents/op-documents-api-1.2-folders-search-items-get.html
   function searchFiles() {
     return new Promise((resolve, reject) => {
-      
-      const uri = `${config.api.documents}/folders/${config.documents.parentFolderId}/search/items?querytext=fOwner<MATCHES>"14001"&fields=metadata`;
-      let xhr = new XMLHttpRequest();
-
-      xhr.open('GET', uri, true);
-      xhr.send();
-
-      xhr.onreadystatechange = function() {
-        if (this.readyState === 4) {
-          if (this.status === 200) {
-            console.log(this.response);
-            return resolve(this.response);
-          } else {
-            return reject({ status: this.status, text: this.statusText })
+      let account = JSON.parse(sessionStorage.getItem('account'));
+      if (account) {
+        const uri = `${config.api.documents}/folders/${config.documents.parentFolderId}/search/items?querytext=fOwnerLoginName<MATCHES>%60${account.email}%60&fields=metadata`;
+        let xhr = new XMLHttpRequest();
+  
+        xhr.open('GET', uri, true);
+        xhr.send();
+  
+        xhr.onreadystatechange = function() {
+          if (this.readyState === 4) {
+            if (this.status === 200) {
+              console.log(this.response);
+              return resolve(this.response);
+            } else {
+              return reject({ status: this.status, text: this.statusText })
+            }
           }
-        }
-      };
-      xhr.onerror = reject
+        };
+        xhr.onerror = reject
+      }
+      else {
+        return reject({ status: 403, text: 'Account not defined' })
+      }
     });
   }
 
