51726ebc2967ac7071f48bf2ff94726f087abed1
john_money <john.money@oracle.com>
2020-12-04 23:31:59 -0500

    PptxGen

--- files/projects/oce-app-sales-enablement/1.0/app-pptxGen.js
+++ files/projects/oce-app-sales-enablement/1.0/app-pptxGen.js
@@ -1,11 +1,23 @@
 /* app-genPptx.js v1.0 */
 (function (window) {
 
-  function genPptx() {
-    console.log('pptxGen');
+  function pptxForm() {
+    // 1. Create a new Presentation
+    let pres = new PptxGenJS();
+
+    // 2. Add a Slide
+    let slide = pres.addSlide();
+
+    // 3. Add one or more objects (Tables, Shapes, Images, Text and Media) to the Slide
+    let textboxText = "Hello World from PptxGenJS!";
+    let textboxOpts = { x: 1, y: 1, color: '363636', fill: { color:'F1F1F1' }, align: "center" };
+    slide.addText(textboxText, textboxOpts);
+
+    // 4. Save the Presentation
+    pres.writeFile("Sample Presentation.pptx");
   }
 
   //main entry
-  document.getElementById('pptxGen').addEventListener('click', genPptx);
+  document.getElementById('pptxGen').addEventListener('click', pptxForm);
 
 })(window);
